89ea36dd8cdba8b6b9e6b48bbfc2ac33bba42d57
gabsgc <gabriellagarccia@gmail.com>
2020-08-17 17:23:12 -0300

    Up PageHeader and Landing

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
@@ -10,11 +10,12 @@ import styles from './styles';
 
 interface PageHeaderProps {
     title: string;
+    headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
     const { navigate } = useNavigation();
-    
+
     function handleGoBack() {
         navigate('Landing');
     }
@@ -23,13 +24,18 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
         <View style={styles.container}>
             <View style={styles.topBar}>
                 <BorderlessButton onPress={handleGoBack}>
-                    <Image source={backIcon} resizeMode="contain"/>
+                    <Image source={backIcon} resizeMode="contain" />
                 </BorderlessButton>
 
                 <Image source={logoImg} resizeMode="contain" />
             </View>
 
-            <Text style={styles.title}>{title}</Text>            
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+                {headerRight}
+            </View>
+
+            {children}
         </View>
     );
 }
